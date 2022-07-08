02e73bc88f5ff1c3de28846ff8e6a60643925621
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-07-08 16:59:38 +0300

    fix

--- app/src/main/java/com/nisaefendioglu/qrcodescanner/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/qrcodescanner/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.nisaefendioglu.qrcodescanner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Size
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import com.nisaefendioglu.qrcodescanner.ui.theme.QRCodeScannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +31,92 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             QRCodeScannerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                var code by remember {
+                    mutableStateOf("")
+                }
+                var hasReadCode by remember {
+                    mutableStateOf(false)
+                }
+                val context = LocalContext.current
+                val lifecycleOwner = LocalLifecycleOwner.current
+                val cameraProviderFeature = remember {
+                    ProcessCameraProvider.getInstance(context)
+                }
+                var hasCamPermission by remember {
+                    mutableStateOf(
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.CAMERA
+                        ) == PackageManager.PERMISSION_GRANTED
+                    )
+                }
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission(),
+                    onResult = { granted ->
+                        hasCamPermission = granted
+                    }
+                )
+                LaunchedEffect(key1 = true) {
+                    launcher.launch(Manifest.permission.CAMERA)
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    if (hasCamPermission) {
+                        if (hasReadCode) {
+                            LoadWebUrl(code)
+                        } else {
+                            AndroidView(
+                                factory = { context ->
+                                    val previewView = PreviewView(context)
+                                    val preview = Preview.Builder().build()
+                                    val selector = CameraSelector.Builder()
+                                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                                        .build()
+                                    preview.setSurfaceProvider(previewView.surfaceProvider)
+                                    val imageAnalysis = ImageAnalysis.Builder()
+                                        .setTargetResolution(Size(
+                                            previewView.width,
+                                            previewView.height
+                                        ))
+                                        .setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
+                                        .build()
+                                    imageAnalysis.setAnalyzer(
+                                        ContextCompat.getMainExecutor(context),
+                                        QRCode { result ->
+                                            code = result
+                                            hasReadCode = true
+                                        }
+                                    )
+                                    try {
+                                        cameraProviderFeature.get().bindToLifecycle(
+                                            lifecycleOwner,
+                                            selector,
+                                            preview,
+                                            imageAnalysis
+                                        )
+                                    } catch (e: Exception) {
+                                        e.printStackTrace()
+                                    }
+                                    previewView
+                                },
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    QRCodeScannerTheme {
-        Greeting("Android")
+    @Composable
+    fun LoadWebUrl(url: String) {
+        AndroidView(factory = {
+            WebView(this).apply {
+                webViewClient = WebViewClient()
+                loadUrl(url)
+            }
+        })
     }
+
 }
